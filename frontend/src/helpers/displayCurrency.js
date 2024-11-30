353c04a6cdf68c275d0ba6cd8c7b592f8c9c3d83
MafuKung <siramet603@gmail.com>
2024-11-30 15:43:51 +0700

    Change currency to Baht

--- frontend/src/helpers/displayCurrency.js
+++ frontend/src/helpers/displayCurrency.js
@@ -1,7 +1,7 @@
 const displayINRCurrency = (num) => {
-    const formatter = new Intl.NumberFormat('en-IN',{
+    const formatter = new Intl.NumberFormat('th-TH',{
         style : "currency",
-        currency : 'INR',
+        currency : 'THB',
         minimumFractionDigits : 2
     })
 
