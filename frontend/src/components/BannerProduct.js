e40c6293f0313a6bdfc6d2f34a93217af3b08ed8
MafuKung <siramet603@gmail.com>
2024-12-05 15:13:22 +0700

    change img
    change banner and logo

--- frontend/src/components/BannerProduct.js
+++ frontend/src/components/BannerProduct.js
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from 'react'
-import image1 from '../assest/banner/img1.webp'
-import image2 from '../assest/banner/img2.webp'
-import image3 from '../assest/banner/img3.jpg'
-import image4 from '../assest/banner/img4.jpg'
-import image5 from '../assest/banner/img5.webp'
+import image1 from '../assest/banner/img_1.webp'
+import image2 from '../assest/banner/img_2.png'
+import image3 from '../assest/banner/img_3.webp'
+import image4 from '../assest/banner/img_4.jpg'
+import image5 from '../assest/banner/img_5.jpg'
 
 
-import image1Mobile from '../assest/banner/img1_mobile.jpg'
-import image2Mobile from '../assest/banner/img2_mobile.webp'
-import image3Mobile from '../assest/banner/img3_mobile.jpg'
-import image4Mobile from '../assest/banner/img4_mobile.jpg'
-import image5Mobile from '../assest/banner/img5_mobile.png'
+import image1Mobile from '../assest/banner/img_1_mobile.jpg'
+import image2Mobile from '../assest/banner/img_2_mobile.jpg'
+import image3Mobile from '../assest/banner/img_3_mobile.avif'
+import image4Mobile from '../assest/banner/img_4_mobile.jpg'
+import image5Mobile from '../assest/banner/img_5_mobile.jpg'
 
 import { FaAngleRight } from "react-icons/fa6";
 import { FaAngleLeft } from "react-icons/fa6";
